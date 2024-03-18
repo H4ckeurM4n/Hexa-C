ba2046a24f2c22936c9a787af30b8f5e440cc5c2
H4ckeurM4n <camille.he@gmx.com>
2024-03-18 15:47:00 +0100

    Amélioration labyrinthe

--- Labyrinthe/Laby.cpp
+++ Labyrinthe/Laby.cpp
@@ -4,28 +4,48 @@
 
 using namespace std;
 
-
 void afficherMap(const vector<vector<char>>& map) {
+    cout << "+---+---+---+---+---+" << endl; // Bordure supérieure
     for (const auto& ligne : map) {
+        cout << "| "; // Bordure gauche
         for (char caseMap : ligne) {
-            cout << caseMap << " ";
+            cout << caseMap << " | "; // Séparateurs de cellule
         }
         cout << endl;
+        cout << "+---+---+---+---+---+" << endl; // Séparateurs de ligne et bordure inférieure
+    }
+}
+
+bool deplacerJoueur(vector<vector<char>>& map, int& posX, int& posY, const string& commande) {
+    int nouvellePosX = posX;
+    int nouvellePosY = posY;
+    
+    if (commande == "haut" && posX > 0) nouvellePosX--;
+    else if (commande == "bas" && posX < map.size() - 1) nouvellePosX++;
+    else if (commande == "gauche" && posY > 0) nouvellePosY--;
+    else if (commande == "droite" && posY < map[0].size() - 1) nouvellePosY++;
+    
+    if (map[nouvellePosX][nouvellePosY] != 'X') {
+        map[posX][posY] = ' ';
+        posX = nouvellePosX;
+        posY = nouvellePosY;
+        map[posX][posY] = 'P';
+        return true;
+    } else {
+        return false;
     }
 }
 
 int main() {
     vector<vector<char>> map = {
         {' ', ' ', ' ', 'X', ' '},
-        {' ', 'X', ' ', 'X', ' '},
+        {'X', 'X', ' ', 'X', 'X'},
         {' ', ' ', ' ', ' ', ' '},
-        {' ', 'X', ' ', 'X', ' '},
-        {' ', ' ', ' ', ' ', 'P'} 
+        {' ', 'X', ' ', 'X', 'X'},
+        {'X', 'X', ' ', ' ', 'P'}
     };
 
-    int posX = 4; 
-    int posY = 4; 
-
+    int posX = 4, posY = 4;
     string commande;
     bool jeuEnCours = true;
 
@@ -34,31 +54,11 @@ int main() {
         cout << "Entrez une commande (haut, bas, droite, gauche) ou 'quitter' pour sortir: ";
         cin >> commande;
 
-        if (commande == "quitter") {
-            jeuEnCours = false;
-            continue;
-        }
+        if (commande == "quitter") break;
 
-
-        int nouvellePosX = posX;
-        int nouvellePosY = posY;
-
-        if (commande == "haut" && posX > 0) nouvellePosX--;
-        else if (commande == "bas" && posX < map.size() - 1) nouvellePosX++;
-        else if (commande == "gauche" && posY > 0) nouvellePosY--;
-        else if (commande == "droite" && posY < map[0].size() - 1) nouvellePosY++;
-
-
-        if (map[nouvellePosX][nouvellePosY] == 'X') {
+        if (!deplacerJoueur(map, posX, posY, commande)) {
             cout << "Vous ne pouvez pas traverser un mur!" << endl;
-            continue;
         }
-
-
-        map[posX][posY] = ' ';
-        posX = nouvellePosX;
-        posY = nouvellePosY;
-        map[posX][posY] = 'P';
     }
 
     return 0;
